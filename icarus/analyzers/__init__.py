d92b0bda22b593ede41d8434af3a455e38423c07
Bilgehan Kösem <bilkos92@gmail.com>
2023-07-25 01:29:35 +0200

    IC-127 design and implement the basedline for layered analysis

--- icarus/analyzers/__init__.py
+++ icarus/analyzers/__init__.py
@@ -4,20 +4,22 @@ from analyzers.indicators import Indicators
 from analyzers.market_classification import MarketClassification
 from analyzers.patterns import Patterns
 from analyzers.support_resistance import SupportResistance
-
+from typing import Dict
 
 class Analyzer(Indicators, TALibIndicators, Patterns, SupportResistance, MarketClassification):
 
-    def __init__(self, config):
-        self.analysis_config = config.get('analysis')
+    def __init__(self, config: Dict):
+        #self.analysis_config = config.get('analysis')
+        self.analysis_layers = config.get('analysis')
         self.time_scales_config = config.get('time_scales')
 
         return
 
 
-    async def analyze(self, data_dict):
+    async def analyze(self, data_dict: Dict[str, Dict]):
+        layer_0 = self.analysis_layers[0]
         analysis_dict=dict()
-        for pair,data_obj in data_dict.items():
+        for pair, data_obj in data_dict.items():
             analysis_obj = dict()
 
             for time_scale, candlesticks in data_obj.items():
@@ -25,14 +27,14 @@ class Analyzer(Indicators, TALibIndicators, Patterns, SupportResistance, MarketC
                 # Generate coroutines
                 indicator_coroutines = []
                 header = '_'
-                indicator_method_names = list(map(lambda orig_string: header + orig_string, self.analysis_config.keys()))
-                indicator_names = list(self.analysis_config.keys())
+                indicator_method_names = list(map(lambda orig_string: header + orig_string, layer_0.keys()))
+                indicator_names = list(layer_0.keys())
 
                 for ind_method, ind_name in zip(indicator_method_names,indicator_names):
                     if hasattr(self, ind_method): 
-                        indicator_coroutines.append(getattr(self, ind_method)(candlesticks, **self.analysis_config.get(ind_name,{})))
+                        indicator_coroutines.append(getattr(self, ind_method)(candlesticks, **layer_0.get(ind_name,{})))
                     elif ind_method[:4] == '_cdl':
-                        indicator_coroutines.append(getattr(self, '_cdl_handler')(candlesticks, ind_method, **self.analysis_config.get(ind_name,{})))
+                        indicator_coroutines.append(getattr(self, '_cdl_handler')(candlesticks, ind_method, **layer_0.get(ind_name,{})))
 
                     else: raise RuntimeError(f'Unknown Analyzer: "{ind_method}"')
 
@@ -44,11 +46,39 @@ class Analyzer(Indicators, TALibIndicators, Patterns, SupportResistance, MarketC
                 # NOTE: Since coroutines are not reuseable, they require to be created in each cycle
                 # NOTE: pd.Series needs to be casted to list
                 stats = dict()
-                for key, value in zip(self.analysis_config.keys(), analysis_output):
+                for key, value in zip(indicator_names, analysis_output):
                     stats[key] = value
                 # Assign "stats" to each "time_scale"
                 analysis_obj[time_scale] = stats
 
             analysis_dict[pair] = analysis_obj
+
+        for layer in self.analysis_layers[1:]:
+            await self.analyze_layer(layer, analysis_dict)
+
+        return analysis_dict
+
+
+    async def analyze_layer(self, layer: Dict, analysis_dict: Dict[str, Dict]):
+
+        for symbol, symbol_analysis in analysis_dict.items():
+            for time_scale, analyzer_result in symbol_analysis.items():
+                # Generate coroutines
+                indicator_coroutines = []
+                header = '_'
+                indicator_method_names = list(map(lambda orig_string: header + orig_string, layer.keys()))
+                indicator_names = list(layer.keys())
+
+                for ind_method, ind_name in zip(indicator_method_names, indicator_names):
+                    if hasattr(self, ind_method): 
+                        indicator_coroutines.append(getattr(self, ind_method)(analyzer_result, **layer.get(ind_name,{})))
+                    elif ind_method[:4] == '_cdl':
+                        indicator_coroutines.append(getattr(self, '_cdl_handler')(analyzer_result, ind_method, **layer.get(ind_name,{})))
+
+                    else: raise RuntimeError(f'Unknown Analyzer: "{ind_method}"')
+
+                analysis_output = list(await asyncio.gather(*indicator_coroutines))
+                for key, value in zip(indicator_names, analysis_output):
+                    analysis_dict[symbol][time_scale][key] = value
 
         return analysis_dict
